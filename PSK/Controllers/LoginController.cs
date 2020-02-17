edfafb71ebd7f008fff8a3be90ab4ddd667c299a
jurciusmantas <jurciusmantas@gmail.com>
2020-02-17 20:13:58 +0200

    login service working

--- PSK/Controllers/LoginController.cs
+++ PSK/Controllers/LoginController.cs
@@ -1,7 +1,24 @@
-﻿namespace PSK.Controllers
-{
-    public class LoginController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
+using PSK.Model.Entities;
+using PSK.Model.Services;
 
+namespace PSK.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public ServerResult<User> Login([FromBody]LoginArgs args)
+        {
+            return _loginService.Login(args);
+        }
     }
 }
