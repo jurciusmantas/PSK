be9346b1c0f4dd15e2290676b1f7975aaf8efaf3
jurciusmantas <jurciusmantas@gmail.com>
2020-02-17 20:49:26 +0200

    added login page

--- PSK/ClientApp/src/App.js
+++ PSK/ClientApp/src/App.js
@@ -4,11 +4,16 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import LoginPage from './components/pages/loginPage';
+
+// export default () => (
+//   <Layout>
+//     <Route exact path='/' component={Home} />
+//     <Route path='/counter' component={Counter} />
+//     <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+//   </Layout>
+// );
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+    <LoginPage/>
+)
