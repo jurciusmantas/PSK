012dc1b35d9452c1fcf4f041b308d939777b0971
lukasbaltramaitis <lukas.baltramaitis@gmail.com>
2020-03-11 19:11:24 +0200

    Revert "Revert "Sukūriau business layer Entities, kurias galima bus panaudoti EntityFramework Code first.""
    This reverts commit 59395a8cc4bc948ac5098724768806d663247a01.

---
+++ PSK.Model/BusinessEntities/Plan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSK.Model.BusinessEntities
+{
+    public class Plan
+    {
+        public int Id { set; get; }
+        public DateTime WorkDate { set; get; }
+
+        public AssignedTopic AssignedTopic { set; get; }
+    }
+}
