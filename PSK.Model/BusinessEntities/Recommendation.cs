a624518b8dca3a92c52b0c2b3c3cb6a166ff6792
lukasbaltramaitis <lukas.baltramaitis@gmail.com>
2020-03-11 19:08:38 +0200

    Sukūriau business layer Entities, kurias galima bus panaudoti EntityFramework Code first.

---
+++ PSK.Model/BusinessEntities/Recommendation.cs
@@ -0,0 +1,16 @@
+﻿using PSK.Model.BussinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSK.Model.BusinessEntities
+{
+    public class Recommendation
+    {
+        public int Id { set; get; }
+        public Topic Topic { set; get; }
+        public Employee RecommendedTo { set; get; }
+
+        public Employee CreatedBy { set; get; }
+    }
+}
---
+++ PSK.Model/BusinessEntities/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSK.Model.BusinessEntities
+{
+    public class Topic
+    {
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public String Description { set; get; }
+        public Topic ParentTopic { set; get; }
+    }
+}
