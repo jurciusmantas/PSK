1dd725d065beae399e2d1595390db56ea2b3a114
tomaBurn <toma.burneikaite@gmail.com>
2020-03-18 11:00:09 +0200

    added button to nav menu

--- PSK.UII/ClientApp/src/components/Layout/NavMenu.js
+++ PSK.UII/ClientApp/src/components/Layout/NavMenu.js
@@ -33,6 +33,9 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/home">Home</NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/invite">Invite</NavLink>
+                </NavItem>
               </ul>
             </Collapse>
           </Container>
