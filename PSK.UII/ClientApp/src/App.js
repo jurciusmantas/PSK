109e06ca3ba849141aef52496a03193c101a6fd1
jurciusmantas <jurciusmantas@gmail.com>
2020-03-27 12:49:31 +0200

    (FRONT_ReduxPersist) persisting state globally

--- PSK.UII/ClientApp/src/App.js
+++ PSK.UII/ClientApp/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import Routes from './routes/Routes';
-import store from './redux/store';
+import getStore from './redux/store';
+
+const { store, persistor } = getStore();
 
 const App = () => (
   <Provider store={store}>
-    <Routes/>
+    <PersistGate loading={null} persistor={persistor}>
+      <Routes/>
+    </PersistGate>
   </Provider>
 );
 
