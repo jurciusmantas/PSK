cc6d0897c0107a92bb145c4ce8b6f28c22d43df3
jurciusmantas <jurciusmantas@gmail.com>
2020-05-31 13:44:36 +0300

    (FRONT_CompletionCheck) Quarter check fix

--- PSK.Model/Helpers/DateTimeHelper.cs
+++ PSK.Model/Helpers/DateTimeHelper.cs
@@ -4,16 +4,6 @@ namespace PSK.Model.Helpers
 {
     public static class DateTimeHelper
     {
-        public static int GetQuarter(this DateTime date)
-        {
-            if (date.Month >= 4 && date.Month <= 6)
-                return 1;
-            else if (date.Month >= 7 && date.Month <= 9)
-                return 2;
-            else if (date.Month >= 10 && date.Month <= 12)
-                return 3;
-            else
-                return 4;
-        }
+        public static int GetQuarter(this DateTime date) => (date.Month + 2) / 3;
     }
 }
